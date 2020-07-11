c7e63a05f16a90ad91ddd8f774bc169a6b29b153
Adam van der Voorn <adamvdvoorn@gmail.com>
2020-07-12 10:57:40 +1200

    small change bc of TrieMap becoming a Lib

--- src/main/java/com/protienperdollar/redone/GlobalData.java
+++ src/main/java/com/protienperdollar/redone/GlobalData.java
@@ -2,7 +2,7 @@ package com.protienperdollar.redone;
 
 import android.app.Application;
 
-import com.protienperdollar.redone.trie.Trie;
+import com.vandeadam.util.TrieMap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +10,7 @@ import java.util.Map;
 class GlobalData extends Application {
     static String savedProductsFN = "saved_products.txt";
     static Map<String, Product> savedProducts = new HashMap<>();
-    static Trie savedProductTrie = new Trie();
+    static TrieMap<Product> savedProductTrie = new TrieMap<>();
 
 
 }
